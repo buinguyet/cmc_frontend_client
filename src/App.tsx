87cf093f49777b1bdef32672aea4249cb518d403
buinguyet <Buianhnguyet10a2@gmail.com>
2022-08-21 11:33:36 +0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import ClientComponent from './components/ClientComponents';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client';
-import DashboardComponent from './components/DashboardComponent';
 import { INITIAL_DATA } from './components/constant';
 
 const socket = io("http://localhost:8000");
@@ -30,13 +28,7 @@ function App() {
 
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={< ClientComponent socket={socket} initialData={INITIAL_DATA} />}></Route>
-          <Route path='/client' element={< ClientComponent socket={socket} initialData={INITIAL_DATA} />}></Route>
-          <Route path='/dashboard' element={< DashboardComponent socket={socket} initialData={INITIAL_DATA} />}></Route>
-        </Routes>
-      </Router>
+      < ClientComponent socket={socket} initialData={INITIAL_DATA} />
     </div>
   )
 }
